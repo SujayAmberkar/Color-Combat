c3e7a5aaea58cc7baac982c2c0c6ccb39e2dc6c7
SujayAmberkar <sujayamberkar@gmail.com>
2022-12-29 15:43:23 +0530

    Gun Recoil added

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Recoil(Input.GetMouseButtonDown(0));
+    }
+
+    void Recoil(bool isFiring){
+        anim.SetBool("isFiring",isFiring);
+    }
+}
--- Assets/Scripts/SwitchGun.cs
+++ Assets/Scripts/SwitchGun.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SwitchGun : MonoBehaviour
 {
+    public List<GameObject> Guns = new List<GameObject>();
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,18 @@ public class SwitchGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        ScrollToSwitchWeapon(scroll);
     }
 
-    void ScrollToSwitchWeapon(){
+    void ScrollToSwitchWeapon(float scroll){
+        if(count>=0 && count<= Guns.Count){
+            if(scroll>0f){
+                count++;
+            }else{
+                count--;
+            }
+        }
         
     }
 }
